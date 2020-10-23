6e7652157fa83dcff078c18b46196c04d803c031
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-23 20:25:25 +0100

    Add selected and change classes favourites shows

--- js/main.js
+++ js/main.js
@@ -2,46 +2,79 @@
 
 const inputElement = document.querySelector(".js-input");
 const btnElement = document.querySelector(".js-btn");
-const listContainerElement = document.querySelector(".js-list-container");
+const listContainer = document.querySelector(".js-list-container");
 
 let shows = [];
+let favShows = [];
 
-// function get data from server
-
+/* get data from server function */
 function getData() {
   let inputValue = inputElement.value;
   fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        shows.push(data[i].show);
-      }
+      shows = data;
       paintData();
+      listenFavourites();
     });
 }
 
-// function paint data search results
-
+/* paint search results and paint selected favourites class function*/
 function paintData() {
   let html = "";
   let defaultImage = "//via.placeholder.com/210x295/ffffff/666666/?text=TV";
   for (let i = 0; i < shows.length; i++) {
-    html += `<li class="section__list--movie"></li>`;
-    html += `<div class="movie__wrapper">`;
-    if (shows[i].image === null) {
-      html += `<img src="${defaultImage}" alt="Imagen de la serie ${shows[i].name}" class="movie__image js-image" />`;
+    let classFav1;
+    let classFav2;
+    const favIndex = favShows.indexOf(i);
+    const fav = favIndex !== -1;
+    if (fav === true) {
+      classFav1 = "show__wrapper--fav";
+      classFav2 = "show__title--fav";
     } else {
-      html += `<img src="${shows[i].image.medium}" alt="Imagen de la serie ${shows[i].name}" class="movie__image js-image" />`;
+      classFav1 = "";
+      classFav2 = "";
     }
-    html += `<h4 class="movie__title">${shows[i].name}</h4>`;
-    html += "</div>";
-    html += "</li>";
+    html += `<li class="section__list--show"></li>`;
+    html += `<div class="show__wrapper js-favourites ${classFav1}" id="${i}">`;
+    if (shows[i].show.image === null) {
+      html += `<img src="${defaultImage}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image js-image" />`;
+    } else {
+      html += `<img src="${shows[i].show.image.medium}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image js-image" />`;
+    }
+    html += `<h4 class="show__title ${classFav2}">${shows[i].show.name}</h4>`;
+    html += `</div>`;
+    html += `</li>`;
   }
-  listContainerElement.innerHTML = html;
+  listContainer.innerHTML = html;
 }
 
-// event
-
+/* listen event form button function */
 btnElement.addEventListener("click", getData);
 
+function favouritesShows(event) {
+  const clickedItem = parseInt(event.currentTarget.id);
+  const indexFav = favShows.indexOf(clickedItem);
+  const isFav = indexFav !== -1;
+
+  if (isFav === false) {
+    console.log("lo meto");
+    favShows.push(clickedItem);
+  } else {
+    console.log("lo saco");
+    favShows.splice(indexFav, 1);
+  }
+  paintData();
+  listenFavourites();
+}
+
+/* listen event favourites function */
+function listenFavourites() {
+  const showsItem = document.querySelectorAll(".js-favourites");
+  for (const show of showsItem) {
+    show.addEventListener("click", favouritesShows);
+  }
+}
+
+/* start web */
 getData();
