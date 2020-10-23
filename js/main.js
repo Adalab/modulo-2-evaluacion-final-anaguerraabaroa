3ac397c158202dadefaedafdc5154575862c4152
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-23 15:53:35 +0100

    Add paintData function

--- js/main.js
+++ js/main.js
@@ -2,25 +2,39 @@
 
 const inputElement = document.querySelector(".js-input");
 const btnElement = document.querySelector(".js-btn");
+const listContainerElement = document.querySelector(".js-list-container");
 
 let shows = [];
-let finalShows = [];
 
 function getData() {
   let inputValue = inputElement.value;
   fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then(function (data) {
-      let showName = "";
-      let showImage = "";
+    .then((response) => response.json())
+    .then((data) => {
       for (let i = 0; i < data.length; i++) {
-        finalShows = data[i].show;
-        showName = data[i].show.name;
-        showImage = data[i].show.image.medium;
+        shows.push(data[i].show);
       }
+      paintData();
     });
 }
 
+function paintData() {
+  let html = "";
+  let defaultImage = "//via.placeholder.com/210x295/ffffff/666666/?text=TV";
+  for (let i = 0; i < shows.length; i++) {
+    html += `<li class="section__list--movie"></li>`;
+    html += `<div class="movie__wrapper">`;
+    if (shows[i].image === null) {
+      html += `<img src="${defaultImage}" alt="Imagen de la serie ${shows[i].name}" class="movie__image js-image" />`;
+    } else {
+      html += `<img src="${shows[i].image.medium}" alt="Imagen de la serie ${shows[i].name}" class="movie__image js-image" />`;
+    }
+    html += `<h4 class="movie__title">${shows[i].name}</h4>`;
+    html += "</div>";
+    html += "</li>";
+  }
+  listContainerElement.innerHTML = html;
+}
+
 btnElement.addEventListener("click", getData);
+getData();
