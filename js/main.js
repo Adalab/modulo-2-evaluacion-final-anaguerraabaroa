b8fc9c31dbde5518b61412961b1615cea1a90e10
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-24 16:22:41 +0100

    Add localstorage function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const inputElement = document.querySelector(".js-input"); // text input element
 const btnElement = document.querySelector(".js-btn"); // search button element
 const listContainer = document.querySelector(".js-list-container"); // search results list element
+const favouriteList = document.querySelector(".js-favourites-list"); // favourite shows list element
 
 /* original array */
 let shows = [];
@@ -18,6 +19,7 @@ function getData() {
     .then((data) => {
       shows = data;
       paintData();
+      listenSearchbtn();
       listenFavourites();
     });
 }
@@ -41,9 +43,9 @@ function paintData() {
     html += `<li class="section__list--show"></li>`;
     html += `<div class="show__wrapper js-favourites ${classFav1}" id="${i}">`;
     if (shows[i].show.image === null) {
-      html += `<img src="${defaultImage}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image js-image"/>`;
+      html += `<img src="${defaultImage}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image"/>`;
     } else {
-      html += `<img src="${shows[i].show.image.medium}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image js-image" />`;
+      html += `<img src="${shows[i].show.image.medium}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image" />`;
     }
     html += `<h4 class="show__title ${classFav2}">${shows[i].show.name}</h4>`;
     html += `</div>`;
@@ -51,9 +53,6 @@ function paintData() {
   }
   listContainer.innerHTML = html;
 }
-
-/* function: listen event (click search button) */
-btnElement.addEventListener("click", getData);
 
 /* function: select favourite shows and add/remove elements to/from favourite shows array */
 function favouriteShows(event) {
@@ -69,29 +68,27 @@ function favouriteShows(event) {
   paintData();
   listenFavourites();
   paintFavShows();
+  setLocalStorage();
 }
 
 /* function: paint favourite shows */
 function paintFavShows() {
-  let html = "";
   let htmlFav = "";
+  let defaultImage = "./images/default_image.jpg";
   for (let i = 0; i < favShows.length; i++) {
     htmlFav += `<li class="section__list--favshow">`;
-    htmlFav += `<div class="favshow__wrapper" id="${i}"></div>`;
-    if (shows[i].show.image === null) {
-      htmlFav += `<img src="${defaultImage}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image js-image"/>`;
+    htmlFav += `<div class="favshow__wrapper" js-favourites id="${i}"></div>`;
+    if (favShows[i].show.image === null) {
+      htmlFav += `<img src="${defaultImage}" alt="Imagen de la serie ${favShows[i].show.name}" class="favshow__image"/>`;
     } else {
-      htmlFav += `<img src="${shows[i].show.image.medium}" alt="Imagen de la serie ${shows[i].show.name}" class="show__image js-image" />`;
+      htmlFav += `<img src="${favShows[i].show.image.medium}" alt="Imagen de la serie ${favShows[i].show.name}" class="favshow__image" />`;
     }
-    htmlFav += ` <h4 class="show__title">${shows[i].show.name}</h4>`;
-    htmlFav += `<button class="show__button"><i class="fas fa-times-circle"></i></button>`;
+    htmlFav += ` <h4 class="favshow__title">${favShows[i].show.name}</h4>`;
+    htmlFav += `<button class="favshow__button"><i class="fas fa-times-circle"></i></button>`;
     htmlFav += `</div>`;
     htmlFav += `</li>`;
   }
-  html += htmlFav;
-  const favouriteList = document.querySelector(".js-favourites-list");
   favouriteList.innerHTML = htmlFav;
-  listenFavourites();
 }
 
 /* function: listen event (click favourite shows) */
@@ -103,6 +100,23 @@ function listenFavourites() {
 }
 
 /* function: save data in LocalStorage */
+function setLocalStorage() {
+  localStorage.setItem("favShows", JSON.stringify(favShows));
+}
+
+/* function: get data from LocalStorage */
+function getLocalStorage() {
+  const localFavShows = localStorage.getItem("favShows");
+  const localFavShowsJS = JSON.parse(localFavShows);
+  if (localFavShows !== null) {
+    favShows = localFavShowsJS;
+  }
+}
+
+/* function: listen event (click search button) */
+function listenSearchbtn() {
+  btnElement.addEventListener("click", getData);
+}
 
 /* start web */
 getData();
